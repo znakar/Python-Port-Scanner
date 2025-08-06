c4c1b08641179d218f3ccf99f750a344ba51a61a
znakar <andreyassassinmishin@gmail.com>
2025-08-07 01:30:22 +0500

    Add multiscan

--- port-scanner.py
+++ port-scanner.py
@@ -1,22 +1,78 @@
 import socket
 import sys
-import time
+import datetime
 from concurrent.futures import ThreadPoolExecutor
 
 
-MAX_WORKERS = 200
+MAX_WORKERS = 100
+
+date_time = datetime.datetime.now()
+
+print(date_time)
 
 host = input("Введите IP-адрес: ")
 port = int(input("Введите порт: "))
 
-port_range = '0-65535'
 
+port_range = '0-1025'
+port_range = port_range.split('-')
+start_port = int(port_range[0])
+end_port = int(port_range[1])
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.settimeout(2)
-    result = s.connect_ex((host, port))
+def scanhost(host_port):
+    host, port = host_port
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(2)
+        result = s.connect_ex((host, port))
     if result == 0:
         print(f"Порт {port}/TCP открыт")
     else:
         print(f"Порт {port}/TCP закрыт")
+    
+scanhost((host, port))
 
+list_of_ports = [(host, port) for port in range(start_port, end_port)]
+
+with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+     executor.map(scanhost, list_of_ports)
+
+
+
+
+
+
+
+# def scanrange(host: str, start_port: int, end_port: int):
+#     for port in range(start_port, end_port):
+#         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         s.settimeout(2)
+        
+
+
+        
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.settimeout(2)
+#     result = s.connect_ex((host, port))
+#     if result == 0:
+#         print(f"Порт {port}/TCP открыт")
+#     else:
+#         print(f"Порт {port}/TCP закрыт")
+
+            
+
+
+    
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.settimeout(2)
+#     result = s.connect_ex((host, port))
+#     if result == 0:
+#         print(f"Порт {port}/TCP открыт")
+#     else:
+#         print(f"Порт {port}/TCP закрыт")
+
+
+
+
+
+# result = socket.connect_ex((host, port))
