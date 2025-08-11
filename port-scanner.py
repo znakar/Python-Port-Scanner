55dc4dbebaeda5abef8fb49e4c6b04afd6043e85
znakar <andreyassassinmishin@gmail.com>
2025-08-11 14:04:06 +0500

    Add scanning UDP ports
    conflict resolved

--- port-scanner.py
+++ port-scanner.py
@@ -4,22 +4,21 @@ import datetime
 from concurrent.futures import ThreadPoolExecutor
 
 
-MAX_WORKERS = 100
-
+MAX_WORKERS = 300
+MESSAGE = b'Ping...'
 date_time = datetime.datetime.now()
 
 print(date_time)
 
 host = input("Введите IP-адрес: ")
-port = int(input("Введите порт: "))
-
-
 port_range = '0-1025'
+
+
 port_range = port_range.split('-')
 start_port = int(port_range[0])
 end_port = int(port_range[1])
 
-def scanhost(host_port):
+def scanhost_tcp(host_port):
     host, port = host_port
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.settimeout(2)
@@ -27,12 +26,29 @@ def scanhost(host_port):
     if result == 0:
         print(f"Порт {port}/TCP открыт")
     else:
-        print(f"Порт {port}/TCP закрыт")
+        pass
     
-scanhost((host, port))
 
-list_of_ports = [(host, port) for port in range(start_port, end_port)]
+def scanhost_udp(host_port):
+    host , port = host_port
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as s:
+        s.settimeout(1)
+        try:
+            s.sendto(MESSAGE, (host, port))
+            print(f"Порт {port}/UDP открыт")
+        except socket.timeout:
+            print(f"Порт {port}/UDP открыт или фильртуется (нет ответа)")
+        except ConnectionRefusedError:
+            print(f"Порт {port}/UDP закрыт")
 
-with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
-     executor.map(scanhost, list_of_ports)
+
+list_of_ports = [(host, port) for port in range(start_port, end_port + 1)]
+
+with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:    
+    tcp_scan = executor.map(scanhost_tcp, list_of_ports)
+    udp_scan = executor.map(scanhost_udp, list_of_ports)
+
+
+     
+
 
